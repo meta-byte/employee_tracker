003322fa362f0b32a11eb87c080468cdb25e2a0b
Garrett Howard <hylander.garrett@gmail.com>
2020-02-19 19:53:56 -0700

    populated departments table with seeds

---
+++ seeds.sql
@@ -0,0 +1,17 @@
+USE employee_management_db;
+
+-- Department Seeds
+INSERT INTO department (name)
+VALUES ("Engineering");
+
+INSERT INTO department (name)
+VALUES ("Human Resources");
+
+INSERT INTO department (name)
+VALUES ("Customer Service");
+
+-- Role Seeds
+
+
+
+-- Employee Seeds
